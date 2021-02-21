b9187f8e4daef24ea82f9a0325ca355f98928274
Fernando Paulino <40306898+xizes2@users.noreply.github.com>
2021-02-21 13:46:09 +0100

    Configure and fix various things related to the IDE, virtual environment and webdriver file.

--- recr_robot.py
+++ recr_robot.py
@@ -1,1 +1,20 @@
 from selenium import webdriver
+
+#Create the 'bridge' with driver, opening the page we want to see and returning the browser element to be used further on.
+#def openwebpage(webpage='https://www.linkedin.com/'):
+#    #Try open the page the 1st time.
+#    try:
+#        browser = webdriver.Edge()
+#        browser.get(webpage)
+#        browser.maximize_window()
+#        return browser
+#    #If we get any error on the process, we cal the function #again.
+#    except (NoSuchWindowException, WebDriverException):
+#        print('Webpage not found, trying again...')
+#        openwebpage()
+
+webpage='https://www.linkedin.com/'
+browser = webdriver.Edge()
+browser.get(webpage)
+#browser.maximize_window() 
+browser.close()       
