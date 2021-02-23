979b4a2819d137e148020f3dacaf19737375a57e
Fernando Paulino <40306898+xizes2@users.noreply.github.com>
2021-02-23 21:47:51 +0100

    finish login and create searchbar functions

--- recr_robot.py
+++ recr_robot.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException, WebDriverException, MoveTargetOutOfBoundsException, NoSuchWindowException
+import keyboard
+from time import sleep
 
 
 #Create the 'bridge' with driver, opening the page we want to see and returning the browser element to be used further on.
@@ -16,20 +18,36 @@ def openWebPage(webpage='https://www.linkedin.com/'):
         #openwebpage()
 
 #Logging in.
-def login(browser, email='email@mail.com', pword='password'):
-    #Find login button, click it, insert login string and click it.
+def login(browser, email='MAIL@mail.com', pword='password'):
     try:        
-        login_button = browser.find_element_by_class_name("nav__button-secondary")
+        #Find login button, click it.
+        login_button = browser.find_element_by_class_name('nav__button-secondary')
         login_button.click()
-        user_button = browser.find_element_by_id("username")
+        #Find user login box, insert login string.
+        user_button = browser.find_element_by_id('username')
         user_button.send_keys(email)
+        #Find user password box, insert password
+        pword = browser.find_element_by_id('password')
+        pword.send_keys('PASSWORD')
+        #Find login button and click to finish login
+        browser.find_element_by_class_name('login__form_action_container').click()
     #If we get any error on the process, we call the function #again.
     except (WebDriverException, AttributeError):
         print('Something wrong here, man. Trying again...')
         #login(browser)
 
+#Access LinkeIn search bar and input the name of the job we want.
+def searchBar(browser):
+    search_bar = browser.find_element_by_id('ember20')
+    search_bar.click()
+    keyboard.write('analista de datos')
+    keyboard.press_and_release('Return')
+
 browser = openWebPage()
+sleep(3)
 login(browser)
+sleep(3)
+searchBar(browser)
 
 
 
--- tests.py
+++ tests.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+import keyboard
 
 webpage='https://www.linkedin.com/'
 browser = webdriver.Edge()
@@ -8,6 +9,17 @@ browser.maximize_window()
 login = browser.find_element_by_class_name("nav__button-secondary")
 login.click()
 user = browser.find_element_by_id("username")#.click()
-user.send_keys("email@hotmail.com")
+user.send_keys("MAIL@mail.com")
 
-#browser.close()       
+pword = browser.find_element_by_id("password")
+pword.send_keys('PASSWORD')
+
+browser.find_element_by_class_name("login__form_action_container").click()
+#enter.click()
+#browser.close()  
+# 
+search_bar = browser.find_element_by_id("ember20")
+search_bar.click()
+#browser.find_element_by_id("ember18").send_keys("data analyst")
+keyboard.write("analista de datos")
+keyboard.press_and_release('Return')
