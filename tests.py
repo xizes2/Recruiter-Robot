4246ac909109dde8bf46ae28f549dd46d2f43e4c
Fernando Paulino <40306898+xizes2@users.noreply.github.com>
2021-02-26 20:57:36 +0100

    more tests and frustration...

--- tests.py
+++ tests.py
@@ -1,25 +1,40 @@
 from selenium import webdriver
-import keyboard
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
 
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import StaleElementReferenceException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions
+
+#Open webpage
 webpage='https://www.linkedin.com/'
 browser = webdriver.Edge()
+browser.implicitly_wait(15)
 browser.get(webpage)
 browser.maximize_window()
- 
-login = browser.find_element_by_class_name("nav__button-secondary")
+
+#Login  
+login = browser.find_element_by_class_name('nav__button-secondary')
 login.click()
-user = browser.find_element_by_id("username")#.click()
-user.send_keys("MAIL@mail.com")
+browser.find_element_by_id('username').send_keys('mail@mail.com')
+browser.find_element_by_id('password').send_keys('password')
+browser.find_element_by_class_name('login__form_action_container').click()
 
-pword = browser.find_element_by_id("password")
-pword.send_keys('PASSWORD')
+#Search keywords
+#search_bar = 
+browser.find_element_by_class_name('search-global-typeahead__input always-show-placeholder'.replace(' ', '.')).send_keys('analista de datos' + Keys.ENTER)
 
-browser.find_element_by_class_name("login__form_action_container").click()
-#enter.click()
-#browser.close()  
-# 
-search_bar = browser.find_element_by_id("ember20")
-search_bar.click()
-#browser.find_element_by_id("ember18").send_keys("data analyst")
-keyboard.write("analista de datos")
-keyboard.press_and_release('Return')
+#Click on 'Jobs' button
+WebDriverWait(browser, 5).until(expected_conditions.presence_of_element_located((By.CLASS_NAME, 'mr2'))).click()
+#browser.find_element_by_class_name('mr2').click()
+
+#Select location
+ActionChains(browser).move_to_element(browser.find_element_by_class_name('jobs-search-box__text-input').click())
+
+
+
+
+   
+#search_boxes[1]
