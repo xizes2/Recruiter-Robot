ccc6c0c51689e417faa7db06d2f57ece203aa3c3
Fernando Paulino <40306898+xizes2@users.noreply.github.com>
2021-02-27 11:56:43 +0100

    finished job selection and location

--- recr_robot.py
+++ recr_robot.py
@@ -1,5 +1,9 @@
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException, WebDriverException, MoveTargetOutOfBoundsException, NoSuchWindowException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 import keyboard
 from time import sleep
 
@@ -18,7 +22,7 @@ def openWebPage(webpage='https://www.linkedin.com/'):
         #openwebpage()
 
 #Logging in.
-def login(browser, email='MAIL@mail.com', pword='password'):
+def login(browser, email='mail@hotmail.com', pword='pass'):
     try:        
         #Find login button, click it.
         login_button = browser.find_element_by_class_name('nav__button-secondary')
@@ -27,8 +31,8 @@ def login(browser, email='MAIL@mail.com', pword='password'):
         user_button = browser.find_element_by_id('username')
         user_button.send_keys(email)
         #Find user password box, insert password
-        pword = browser.find_element_by_id('password')
-        pword.send_keys('PASSWORD')
+        password = browser.find_element_by_id('password')
+        password.send_keys(pword)
         #Find login button and click to finish login
         browser.find_element_by_class_name('login__form_action_container').click()
     #If we get any error on the process, we call the function #again.
@@ -38,16 +42,30 @@ def login(browser, email='MAIL@mail.com', pword='password'):
 
 #Access LinkeIn search bar and input the name of the job we want.
 def searchBar(browser):
-    search_bar = browser.find_element_by_id('ember20')
-    search_bar.click()
-    keyboard.write('analista de datos')
-    keyboard.press_and_release('Return')
+    WebDriverWait(browser, 5).until(expected_conditions.presence_of_element_located((By.CLASS_NAME, 'search-global-typeahead__input always-show-placeholder'.replace(' ', '.')))).send_keys('analista de datos' + Keys.ENTER)
+    #Click on 'Jobs' button
+    WebDriverWait(browser, 5).until(expected_conditions.presence_of_element_located((By.CLASS_NAME, 'mr2'))).click()
+
+
+def locationBar(browser, location='Barcelona'):
+    #Access location bar
+    browser.find_element_by_class_name('jobs-search-box__input.jobs-search-box__input--location').click()
+    #Double click the bar to select the default word
+    webdriver.ActionChains(browser).double_click(browser.find_element_by_class_name ('jobs-search-box__input.jobs-search-box__input--location')).perform()
+    #Erase it
+    keyboard.press_and_release('backspace')
+    #Write the location I want
+    keyboard.write('Barcelona')
+    #Press Enter
+    keyboard.press_and_release('return')
 
 browser = openWebPage()
 sleep(3)
 login(browser)
 sleep(3)
 searchBar(browser)
+sleep(3)
+locationBar(browser)
 
 
 
--- tests.py
+++ tests.py
@@ -1,12 +1,13 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
-
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import StaleElementReferenceException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions
+import keyboard
+from time import sleep
 
 #Open webpage
 webpage='https://www.linkedin.com/'
@@ -18,23 +19,25 @@ browser.maximize_window()
 #Login  
 login = browser.find_element_by_class_name('nav__button-secondary')
 login.click()
-browser.find_element_by_id('username').send_keys('mail@mail.com')
-browser.find_element_by_id('password').send_keys('password')
+browser.find_element_by_id('username').send_keys('mail@hotmail.com')
+browser.find_element_by_id('password').send_keys('pass')
 browser.find_element_by_class_name('login__form_action_container').click()
 
 #Search keywords
 #search_bar = 
-browser.find_element_by_class_name('search-global-typeahead__input always-show-placeholder'.replace(' ', '.')).send_keys('analista de datos' + Keys.ENTER)
+WebDriverWait(browser, 5).until(expected_conditions.presence_of_element_located((By.CLASS_NAME, 'search-global-typeahead__input always-show-placeholder'.replace(' ', '.')))).send_keys('analista de datos' + Keys.ENTER)
 
 #Click on 'Jobs' button
 WebDriverWait(browser, 5).until(expected_conditions.presence_of_element_located((By.CLASS_NAME, 'mr2'))).click()
-#browser.find_element_by_class_name('mr2').click()
 
 #Select location
-ActionChains(browser).move_to_element(browser.find_element_by_class_name('jobs-search-box__text-input').click())
-
-
-
-
-   
-#search_boxes[1]
+#Access location bar
+browser.find_element_by_class_name('jobs-search-box__input.jobs-search-box__input--location').click()
+#Double click the bar to select the default word
+webdriver.ActionChains(browser).double_click(browser.find_element_by_class_name('jobs-search-box__input.jobs-search-box__input--location')).perform()
+#Erase it
+keyboard.press_and_release('backspace')
+#Write the location I want
+keyboard.write('Barcelona')
+#Press Enter
+keyboard.press_and_release('return')
